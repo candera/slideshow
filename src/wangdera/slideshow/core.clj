b6c467d58d1f5b84f296c5d65ba4dc41d84baaf7
Craig Andera <candera@wangdera.com>
2010-11-22 08:49:43 -0500

    Initial commit

---
+++ src/wangdera/slideshow/core.clj
@@ -0,0 +1,1 @@
+(ns wangdera.slideshow.core)
---
+++ test/wangdera/slideshow/test/core.clj
@@ -0,0 +1,6 @@
+(ns wangdera.slideshow.test.core
+  (:use [wangdera.slideshow.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
