b25ee84b0947a56500673c4845da06e0bcdedcc4
Craig Andera <candera@osgiliath.(none)>
2010-12-09 07:40:17 -0500

    Add :keep-non-project-classes to solve problem with uberjar not running

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
-  :main wangdera.slideshow.core)
+  :main wangdera.slideshow.core
+  ;; This next option has to do with Clojure bug #322. Something
+  ;; about transitive AOT results in not enough going into the
+  ;; uberjar, which means it won't run.
+  :keep-non-project-classes true)
